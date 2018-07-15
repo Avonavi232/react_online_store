0325665c25e63256243d799cfc10f7a0e08e39da
Avonavi <Avonavi232@gmail.com>
2018-07-15 11:20:42 +0300

    project start   * styles & fonts & imgs moved to src dir

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// regular imports
+ReactDOM.render(<App /> , document.getElementById('root'));
+
+if (module.hot) {
+	module.hot.accept('./App', () => {
+		ReactDOM.render(<App />, document.getElementById('root'))
+	})
+}
+
